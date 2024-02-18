9a56986f37eb08a9763f94a2f4943f993e0d8819
Bryan Quintana <bryandise@gmail.com>
2024-02-18 17:05:45 -0300

    Update next.config.js with new image remote pattern and exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'cdn.leonardo.ai',
+    images: {
+      remotePatterns: [
+        {
+          hostname: 'cdn.leonardo.ai',
+        }
+      ],
+    },
+    exportPathMap:function(){
+      return{
+        '/':{page:'/login'},
       }
-    ],
-  },
-  basePath: '/login',
+  }
 }
