635864adf66bf9774256efa6ef32df9d89072aeb
Bryan Quintana <bryandise@gmail.com>
2024-02-19 18:37:14 -0300

    Add pattern validation for amount input field

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -77,6 +77,7 @@ export default function EditInvoiceForm({
                 className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
                 aria-describedby='amount-error'
                 min={0}
+                pattern='^\d+(?:\.\d{1,2})?$'
               />
               <CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
