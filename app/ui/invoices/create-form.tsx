8a40cb802245122a817d297c78a2b994f6274cc9
Bryan Quintana <bryandise@gmail.com>
2024-02-21 09:22:33 -0300

    Refactor invoice creation page and form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,10 +8,12 @@ import {
   UserCircleIcon
 } from '@heroicons/react/24/outline'
 import { Button } from '@/app/ui/button'
-import { createInvoice } from '@/app/lib/actions'
+import { createInvoice, fetchCustomers } from '@/app/lib/actions'
 import { useFormState } from 'react-dom'
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default async function Form() {
+  const customers: CustomerField[] = await fetchCustomers()
+
   console.log(customers);
 
   const initialState = { message: null, errors: {} }
